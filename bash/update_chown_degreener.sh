f0bf07b05bf37db7b2f6c0a132f1b55d48850490
dylanxhernandez <dylan.x.h.developer@gmail.com>
2023-09-11 20:55:00 -0400

    feat: add a degreener for file highlighting/chown

---
+++ bash/update_chown_degreener.sh
@@ -0,0 +1,4 @@
+# Use this command to update the highlighting on directories and files
+# This will update the ownership and permissions of the file and/or directory specified
+# Note, be aware that you are chaning permissions. Use at your own risk.
+sudo chmod -R a-x,o-w,+X <directory/filename>
