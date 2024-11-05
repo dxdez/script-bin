bd958895a1a1ed622e5724fd50f806b5d4e21b1d
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-11-05 17:05:53 -0500

    chore: remove old files

--- bash/update_chown_degreener.sh
+++
@@ -1,4 +0,0 @@
-# Use this command to update the highlighting on directories and files
-# This will update the ownership and permissions of the file and/or directory specified
-# Note, be aware that you are chaning permissions. Use at your own risk.
-sudo chmod -R a-x,o-w,+X <directory/filename>
